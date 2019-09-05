120844f85ccbbb072de4065d6b7673936c9cc234
Akhila N <54149961+Akhila474@users.noreply.github.com>
2019-09-05 23:43:19 +0530

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,8 @@
+#Write a python program to create a tuple with numbers and print one item
+#Convert this tuple to a list
+
+tuple = 5, 10, 15, 20, 25
+print(tuple)
+print(tuple[4])
+l=list(tuple)
+print("After converting to a list : ",l)
